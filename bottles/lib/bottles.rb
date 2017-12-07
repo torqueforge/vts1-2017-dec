dc83c07cacc8b448e1e7ed6cd5650481fa5dfd99
Sandi Metz <sandi@sandimetz.com>
2017-12-07 13:32:18 -0500

    bottles factories

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -8,18 +8,58 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number = BottleNumber.new(number)
-    next_bottle_number = BottleNumber.new(bottle_number.successor)
+    bottle_number = BottleNumber.for(number)
 
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" +
     "#{bottle_number.action}, " +
-    "#{next_bottle_number} of beer on the wall.\n"
+    "#{bottle_number.successor} of beer on the wall.\n"
   end
 end
 
 
 class BottleNumber
+  def self.for(number)
+    registry.find {|candidate| candidate.handles?(number)}.new(number)
+
+    # @@registry.find {|candidate| candidate.handles?(number)}.new(number)
+
+    # [BottleNumber1, BottleNumber0, BottleNumber].find {|candidate| candidate.handles?(number)}.new(number)
+
+    # Hash.new(BottleNumber).merge(0 => BottleNumber0, 1 => BottleNumber1)[number].new(number)
+
+    # begin
+    #   const_get("BottleNumber#{number}")
+    # rescue NameError
+    #   BottleNumber
+    # end.new(number)
+
+    # case number
+    # when 0
+    #   BottleNumber0
+    # when 1
+    #   BottleNumber1
+    # else
+    #   BottleNumber
+    # end.new(number)
+  end
+
+  def self.handles?(number)
+    true
+  end
+
+  # def self.inherited(candidate)
+  #   register(candidate)
+  # end
+
+  def self.registry
+    @@registry ||= [BottleNumber]
+  end
+
+  def self.register(candidate)
+    registry.unshift(candidate)
+  end
+
   attr_reader :number
   def initialize(number)
     @number = number
@@ -30,42 +70,58 @@ class BottleNumber
   end
 
   def quantity
-    if number == 0
-      "no more"
-    else
-      number.to_s
-    end
+    number.to_s
   end
 
   def container
-    if number == 1
-      "bottle"
-     else
-      "bottles"
-     end
+    "bottles"
   end
 
   def action
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun} down and pass it around"
-    end
+    "Take #{pronoun} down and pass it around"
   end
 
   def pronoun
-    if number == 1
-      "it"
-    else
-      "one"
-    end
+    "one"
   end
 
   def successor
-    if number == 0
-      99
-    else
-      number - 1
-    end
+    BottleNumber.for(number - 1)
+  end
+end
+
+class BottleNumber0 < BottleNumber
+  register(self)
+
+  def self.handles?(number)
+    number == 0
+  end
+
+  def quantity
+    "no more"
+  end
+
+  def action
+    "Go to the store and buy some more"
+  end
+
+  def successor
+    BottleNumber.for(99)
+  end
+end
+
+class BottleNumber1 < BottleNumber
+  register(self)
+
+  def self.handles?(number)
+    number == 1
+  end
+
+  def container
+    "bottle"
+  end
+
+  def pronoun
+    "it"
   end
 end
